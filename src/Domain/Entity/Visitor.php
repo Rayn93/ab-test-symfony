9a048aa576e264fd5b7d75095173d377c0ea5757
robertsaternus <robert.saternus@landingi.com>
2019-12-03 20:10:49 +0100

    Create Visitor Entity

---
+++ src/Domain/Entity/Visitor.php
@@ -0,0 +1,90 @@
+<?php
+namespace Domain\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="Domain\Repository\VisitorRepository")
+ */
+class Visitor
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=120, unique=true)
+     */
+    private $gId;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private $variant;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $vote;
+
+    /**
+     * @ORM\Column(type="string", length=120, nullable=true)
+     */
+    private $country;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGId(): ?string
+    {
+        return $this->gId;
+    }
+
+    public function setGId(string $gId): self
+    {
+        $this->gId = $gId;
+
+        return $this;
+    }
+
+    public function getVariant(): ?string
+    {
+        return $this->variant;
+    }
+
+    public function setVariant(string $variant): self
+    {
+        $this->variant = $variant;
+
+        return $this;
+    }
+
+    public function getVote(): ?int
+    {
+        return $this->vote;
+    }
+
+    public function setVote(?int $vote): self
+    {
+        $this->vote = $vote;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
